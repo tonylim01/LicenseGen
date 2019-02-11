0e50ea6180e53d9d638ccc3088a81f53f6574651
jongchullim <tonylim@uangel.com>
2019-02-11 20:08:53 +0900

    License 파일 프로세스 Argument로 처리하게 변경

--- src/main/java/LicenseGen.java
+++ src/main/java/LicenseGen.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LicenseGen {
-
     public static void main( String[] args ) {
         try {
             AES256Util aes256Util = new AES256Util();
             String enc = null;
             if(args[0].equals( "gen" )) {
-                String raw = aes256Util.licenseTextRead( "/Users/jongchullim/IdeaProjects/ACS_A2S/MediaLicense.txt" );
+                String raw = aes256Util.licenseTextRead( args[1] );
                 System.out.println( "Raw : " + raw );
                 enc = aes256Util.encrypt( raw );
-                aes256Util.licenseFileCreate( "/Users/jongchullim/IdeaProjects/ACS_A2S/.medialicensefile.txt", enc );
+                aes256Util.licenseFileCreate( args[2], enc );
             }
             else if(args[0].equals( "dec" )) {
-                enc = aes256Util.aes256fileread( "/Users/jongchullim/IdeaProjects/ACS_A2S/.medialicensefile.txt" );
+                enc = aes256Util.aes256fileread( args[2] );
                 System.out.println( enc );
                 String dec = aes256Util.decrypt( enc );
                 System.out.println( dec );
